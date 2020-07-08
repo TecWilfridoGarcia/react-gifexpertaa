dd9e63c1a349fd5187bf7a12c221c729cae70be1
Wilfrido Garcia Villa <tec.wilfrido@gmail.com>
2020-07-08 12:05:53 -0500

    add docs

---
+++ docs/precache-manifest.8e4c0937db7d1b0362a0aeecd1a5ff57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91132807c30830c0e2f8cea211114715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfb460962fdcd21159ab",
+    "url": "/static/css/main.26a0a770.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb460962fdcd21159ab",
+    "url": "/static/js/main.b8ef441a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
